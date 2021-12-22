a6e8dd6cded71a75aeba3d1328f750335c62231c
EvgeniaOv <gralph@rambler.ru>
2021-12-22 21:40:19 +0300

    shouldFindSelenideWiki Done

--- src/test/java/test/SelenideWiki.java
+++ src/test/java/test/SelenideWiki.java
@@ -3,6 +3,8 @@ package test;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -16,12 +18,9 @@ public class SelenideWiki {
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
         //клик на раздел Wiki
         $(byText("Wikis")).click();
-        //в списке элементов страницы найти SoftAssertions. клик на страницу SoftAssertions
-        $(byText("SoftAssertions")).click();
+        //в списке элементов страницы должно быть SoftAssertions. клик на страницу SoftAssertions
+        $(byLinkText("SoftAssertions")).shouldBe(visible).click();
         //проверить, что внутри есть пример кода для JUnit5
         $("#wiki-content"). shouldHave(text("Using JUnit5 extend test class:"));
-
-
-        sleep(5000);
     }
 }
