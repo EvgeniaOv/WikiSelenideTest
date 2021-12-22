9c9a2cbccf2fce616d926fc94acc80e2d832579e
EvgeniaOv <gralph@rambler.ru>
2021-12-22 15:29:16 +0300

    auto test start

---
+++ src/test/java/test/SelenideWiki.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+public class SelenideWiki {
+
+    @Test
+    void shouldFindSelenideWiki() {
+       //открыть страницу гитхаба в бразере
+
+       //в поисковой строке ввести Selenide. Ентер
+
+       //клие на раздел Wiki
+
+       //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+
+       //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
+    }
+}
