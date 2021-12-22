75a17ef05de2c67a3d15048cda7c1772e8f9eb91
EvgeniaOv <gralph@rambler.ru>
2021-12-22 20:22:25 +0300

    Wikis

--- src/test/java/test/SelenideWiki.java
+++ src/test/java/test/SelenideWiki.java
@@ -2,6 +2,7 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWiki {
@@ -13,14 +14,14 @@ public class SelenideWiki {
 
         //в поисковой строке ввести Selenide кликнуть Ентер
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
+        //клик на раздел Wiki
+        $(byText("Wikis")).click();
+
+
+
+        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
 
         sleep(5000);
-
-       //клие на раздел Wiki
-
-       //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-
-       //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
-
     }
 }
