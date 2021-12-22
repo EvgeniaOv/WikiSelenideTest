dd1f1367adaa1bced4347bc9ac3537600ed4934e
EvgeniaOv <gralph@rambler.ru>
2021-12-22 21:22:06 +0300

    check код для JUnit5

--- src/test/java/test/SelenideWiki.java
+++ src/test/java/test/SelenideWiki.java
@@ -2,6 +2,7 @@ package test;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,16 +12,15 @@ public class SelenideWiki {
     void shouldFindSelenideWiki() {
        //открыть страницу гитхаба в бразере
         open("https://github.com/");
-
         //в поисковой строке ввести Selenide кликнуть Ентер
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
         //клик на раздел Wiki
         $(byText("Wikis")).click();
-        //в списке элементов страницы найти SoftAssertions
+        //в списке элементов страницы найти SoftAssertions. клик на страницу SoftAssertions
         $(byText("SoftAssertions")).click();
+        //проверить, что внутри есть пример кода для JUnit5
+        $("#wiki-content"). shouldHave(text("Using JUnit5 extend test class:"));
 
-
-        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
 
         sleep(5000);
     }
