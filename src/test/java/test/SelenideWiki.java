f4ccf37dc0b9771fbe1f06289373cacb56d3da8c
EvgeniaOv <gralph@rambler.ru>
2021-12-24 15:08:08 +0300

    shouldFindSelenideWiki CorrectDone

--- src/test/java/test/SelenideWiki.java
+++ src/test/java/test/SelenideWiki.java
@@ -9,18 +9,23 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWiki {
+//  задание 1: 1.$("h1 div") - поиск первого вложенного div внутри h1
+//$("h1").$("div") - поиск сначла перевого h1, а  внем первого div
 
+    //задание 2:
     @Test
     void shouldFindSelenideWiki() {
-       //открыть страницу гитхаба в бразере
+        //открыть страницу гитхаба в бразере
         open("https://github.com/");
         //в поисковой строке ввести Selenide кликнуть Ентер
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
         //клик на раздел Wiki
         $(byText("Wikis")).click();
         //в списке элементов страницы должно быть SoftAssertions. клик на страницу SoftAssertions
-        $(byLinkText("SoftAssertions")).shouldBe(visible).click();
+       // $(byLinkText("SoftAssertions")).shouldBe(visible).click();
+        $("#wiki_search_results").shouldHave(text("SoftAssertions"));
+        $("[title='SoftAssertions']").click();
         //проверить, что внутри есть пример кода для JUnit5
-        $("#wiki-content"). shouldHave(text("Using JUnit5 extend test class:"));
+        $("#wiki-content").shouldHave(text("Using JUnit5 extend test class:"));
     }
-}
+    }
